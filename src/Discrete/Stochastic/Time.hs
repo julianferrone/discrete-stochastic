46120547373ac0a2a9e55ea1d4b9c00bc2d560ff
jules <81225111+jules@users.noreply.github.com>
2025-07-30 21:59:22 +1000

    Split up Events into a list of Event

--- src/Discrete/Stochastic/Time.hs
+++ src/Discrete/Stochastic/Time.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Discrete.Stochastic.Time (Events (..)) where
+
+import Data.Ord (Down, Ordering (..))
 
 ------------------------------------------------------------
 --                         Events                         --
@@ -12,10 +16,19 @@ data TimeStep = Finite Int | Infinite deriving (Eq, Ord, Show)
 
 ----------                 Events                 ----------
 
+newtype Event a = Event {unEvent :: (Down TimeStep, a)} deriving (Eq, Show)
+
+instance Functor Event where
+  fmap f = Event . fmap f . unEvent
+  (<$) f = Event . (<$) f . unEvent
+
 -- | Events is a sequence of values, tagged with the timesteps
 -- at which they happened.
-newtype Events a = Events {unEvents :: [(TimeStep, a)]} deriving (Eq, Show)
+newtype Events a = Events {unEvents :: [Event a]} deriving (Eq, Show)
 
 instance Functor Events where
   fmap f = Events . (fmap . fmap) f . unEvents
   (<$) f = Events . (fmap . (<$)) f . unEvents
+
+-- instance Semigroup (Events a) where
+--   earlier <> later = Events $ unEvents earlier <> unEvents later
