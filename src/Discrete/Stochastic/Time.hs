dc4835263efdd390999294b5503d2d05aa2b1637
jules <81225111+jules@users.noreply.github.com>
2025-07-30 21:31:44 +1000

    Add docstrings

--- src/Discrete/Stochastic/Time.hs
+++ src/Discrete/Stochastic/Time.hs
@@ -1,4 +1,4 @@
-module Discrete.Stochastic.Time () where
+module Discrete.Stochastic.Time (Events(..)) where
 
 ------------------------------------------------------------
 --                         Events                         --
@@ -7,7 +7,14 @@ module Discrete.Stochastic.Time () where
 ----------                  Time                  ----------
 
 data DiscreteTime = Finite Int | Infinite
+-- DiscreteTime is a representation of a discrete timestep.
+-- It can be either a specific timestep `Finite Int`, or a timestep infinitely
+-- far into the future. 
+
 
 ----------                 Events                 ----------
 
 newtype Events a = Events {unEvents :: [(DiscreteTime, a)]}
+-- Events is a sequence of values, tagged with the timesteps 
+-- at which they happened.
+
