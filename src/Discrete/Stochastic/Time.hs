673269cb6f6c3d8b21d64c0cd8ed1cb20d9eece0
jules <81225111+jules@users.noreply.github.com>
2025-07-30 22:14:01 +1000

    Add mergeSorted to merge two sorted lists together

--- src/Discrete/Stochastic/Time.hs
+++ src/Discrete/Stochastic/Time.hs
@@ -28,9 +28,21 @@ instance Functor Event where
 newtype Events a = Events {unEvents :: [Event a]} deriving (Eq, Show)
 
 -- | Smart constructor that ensures the list of Events is sorted.
-events :: Ord a => [Event a] -> Events a
+events :: (Ord a) => [Event a] -> Events a
 events es = Events $ sort es
 
 instance Functor Events where
   fmap f = Events . (fmap . fmap) f . unEvents
   (<$) f = Events . (fmap . (<$)) f . unEvents
+
+-- | Merges two sorted lists into a sorted list.
+mergeSorted :: Ord a => [a] -> [a] -> [a]
+mergeSorted = go []
+  where
+    go :: Ord a => [a] -> [a] -> [a] -> [a]
+    go done [] [] = reverse done            -- No more items to add from either input
+    go done [] ys = go (done <> ys) [] []   -- No more items to add from the left input
+    go done xs [] = go (done <> xs) [] []   -- No more items to add from the right input
+    go done xs@(x : xtail) ys@(y : ytail)   -- Check which head is smaller
+      | x <= y = go (x : done) xtail ys
+      | otherwise = go (y : done) xs ytail
