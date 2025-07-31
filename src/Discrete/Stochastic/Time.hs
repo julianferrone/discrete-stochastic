25d4090a9fa276573edc887ae237bb8183dfa63a
jules <81225111+jules@users.noreply.github.com>
2025-08-01 09:50:10 +1000

    Remove sorted list denotation of time processes

--- src/Discrete/Stochastic/Time.hs
+++ src/Discrete/Stochastic/Time.hs
@@ -1,7 +1,6 @@
-module Discrete.Stochastic.Time (Events (..)) where
+module Discrete.Stochastic.Time () where
 
 import qualified Data.Ord as Ord
-import qualified Discrete.Stochastic.SortedList as SortedList
 
 ------------------------------------------------------------
 --                         Events                         --
@@ -13,27 +12,3 @@ import qualified Discrete.Stochastic.SortedList as SortedList
 -- finite timestep, or a timestep infinitely far into the future.
 data TimeStep = Finite Int | Infinite deriving (Eq, Ord, Show)
 
-----------                 Events                 ----------
-
-newtype Event a = Event {unEvent :: (Ord.Down TimeStep, a)} deriving (Eq, Ord, Show)
-
-instance Functor Event where
-  fmap f = Event . fmap f . unEvent
-  (<$) f = Event . (<$) f . unEvent
-
--- | Events is a sequence of values, tagged with the timesteps
--- at which they happened.
-newtype Events a = Events {unEvents :: SortedList.SortedList (Event a)} deriving (Eq, Show)
-
--- | Smart constructor that ensures the list of Events is sorted.
-events :: (Ord a) => [Event a] -> Events a
-events es = Events $ SortedList.fromList es
-
-map :: (Ord b) => (a -> b) -> Events a -> Events b
-map f (Events xs) = Events $ SortedList.map (fmap f) xs
-
-instance Semigroup (Events a) where
-  e1 <> e2 = Events $ unEvents e1 <> unEvents e2
-
-instance (Ord a) => Monoid (Events a) where
-  mempty = Events {unEvents = mempty}
