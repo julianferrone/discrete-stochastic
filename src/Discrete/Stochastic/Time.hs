8fffbfb5314f3c074848d6f2c39c0461e3531e84
jules <81225111+jules@users.noreply.github.com>
2025-08-01 11:31:06 +1000

    Add Semiring typeclass and instances

--- src/Discrete/Stochastic/Time.hs
+++ src/Discrete/Stochastic/Time.hs
@@ -6,6 +6,32 @@ import qualified Data.Ord as Ord
 --                         Events                         --
 ------------------------------------------------------------
 
+----------               Typeclasses              ----------
+
+class Semiring r where
+  nil :: r
+  unit :: r
+  plus :: r -> r -> r
+  times :: r -> r -> r
+
+instance Semiring Int where
+  nil = 0
+  unit = 1
+  plus = (+)
+  times = (*)
+
+instance Semiring Double where
+  nil = 0
+  unit = 1
+  plus = (+)
+  times = (*)
+
+instance (Semiring b) => Semiring (a -> b) where
+  nil = const nil
+  unit = const unit
+  plus f g x = f x `plus` g x
+  times f g x = f x `times` g x
+
 ----------                  Time                  ----------
 
 -- | Type of discrete timestep. It can be either a specific,
