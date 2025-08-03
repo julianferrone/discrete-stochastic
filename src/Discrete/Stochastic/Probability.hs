a8c15d27be06ebb1c05e4dc01170e9bad1d541c1
jules <81225111+jules@users.noreply.github.com>
2025-08-03 13:01:52 +1000

    Add functions to create and work with Prob

--- src/Discrete/Stochastic/Probability.hs
+++ src/Discrete/Stochastic/Probability.hs
@@ -12,6 +12,18 @@ clamp f c d = max f $ min c d
 prob :: Double -> Prob
 prob = Prob . clamp 0 1
 
+almostSurely :: Prob
+almostSurely = Prob 1
+
+almostNever :: Prob
+almostNever = Prob 0
+
+not :: Prob -> Prob
+not (Prob p) = Prob $ 1 - p
+
+(&) :: Prob -> Prob -> Prob
+(Prob p1) & (Prob p2) = Prob (p1 * p2)
+
 ------------------------------------------------------------
 --                Probability Distribution                --
 ------------------------------------------------------------
