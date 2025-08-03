17711ef585fd016411b32fa68435328b333120d4
jules <81225111+jules@users.noreply.github.com>
2025-08-03 13:00:03 +1000

    Rename clamp arguments to not shadow pre-existing prelude bindings

--- src/Discrete/Stochastic/Probability.hs
+++ src/Discrete/Stochastic/Probability.hs
@@ -7,7 +7,7 @@ module Discrete.Stochastic.Probability where
 newtype Prob = Prob Double
 
 clamp :: Double -> Double -> Double -> Double
-clamp floor ceiling d = max floor $ min ceiling d
+clamp f c d = max f $ min c d
 
 prob :: Double -> Prob
 prob = Prob . clamp 0 1
